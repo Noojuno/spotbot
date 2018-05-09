e8f0ee0c707efc70fde3eaaf96711cc0e458c4a5
Noojuno <jonathankemball@gmail.com>
2018-05-09 12:32:28 +1200

    Initial API gateway response formatting

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const SpotifyWebApi = require("spotify-web-api-node");
+const generateApiResponse = (status, body) => {
+  return {
+    statusCode: status,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body)
+  };
+};
 
 exports.handler = async event => {
+  if (!event.queryStringParameters || !event.queryStringParameters.platform) {
+    return generateApiResponse(404, { message: "Invalid platform" });
+  }
+
+  // TODO implement
   console.log(event);
-  return "Hello from Lambda!";
+  return generateApiResponse(200, event);
 };
